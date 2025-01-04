c83b2c13f54f7bfaedb2615d4c7c0358e701f3e3
Tejas Madrewar <tejas.madrewar@gmail.com>
2025-01-04 17:10:25 +0530

    handle invalid paramater exceptions

--- src/main/java/com/nuclear/realworld/api/exception/ApiExceptionHandler.java
+++ src/main/java/com/nuclear/realworld/api/exception/ApiExceptionHandler.java
@@ -2,9 +2,13 @@ package com.nuclear.realworld.api.exception;
 
 import com.nuclear.realworld.domain.exception.EmailNotAvilableException;
 import com.nuclear.realworld.domain.exception.UsernameNotAvilableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -17,5 +21,26 @@ public class ApiExceptionHandler {
     @ExceptionHandler(EmailNotAvilableException.class)
     public String handleEmailAlreadyExistsException(EmailNotAvilableException ex) {
         return "Error: email already exists.";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorMsg handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getAllErrors()
+                .forEach(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+        return new ErrorMsg(errors);
+    }
+
+    public class ErrorMsg {
+        final public Map<String, String> errors;
+        ErrorMsg(Map<String, String> errors) {
+            this.errors = errors;
+        }
     }
 }
